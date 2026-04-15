188eaa9555421a8de956b712697ca163f7443d8b
Priyanka <2501730192@krmu.edu.in>
2026-04-15 15:45:42 +0530

    styled-components , @emotion/styled , @emotion/react ,Tailwind CSS

--- reactapp/src/component/Author.jsx
+++ reactapp/src/component/Author.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+//import styled from 'styled-components'
+import styled from '@emotion/styled'
+import {css} from '@emotion/react'
+
 
 export class Author extends Component {
 
@@ -17,11 +21,26 @@ export class Author extends Component {
         <p>This author is tired !!!!!!!!!!!!!!!!!!1</p>
         <p>Count: {this.state.count}</p>
         <button onClick={this.handleIncrement}>Increment</button>
+        <Button>Click Me</Button>
+        <Button red >Click Again</Button>
+        <button css ={{backgroundColor:'chartreuse' ,color:'green',padding:'100px'}}>Dayumm !!</button>
+        <button className ='bg-pink-500 text-white border-2 border-white'>TailWinddddd !!</button>
       </div>
     )
   }
+
 
   
 }
 
 export default Author
+
+const Button =styled.button`
+  background-color :${props=>props.red ? "red":"pink"} ;
+  color : white;
+  padding:15px;
+
+
+
+`
+
