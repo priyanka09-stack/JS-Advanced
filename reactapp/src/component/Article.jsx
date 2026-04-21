ac604d863ff9d2ce840a4c1d79f8cb839ba08b65
Priyanka <2501730192@krmu.edu.in>
2026-04-21 12:39:13 +0530

    Loading ,State & Axios

--- reactapp/src/component/Article.jsx
+++ reactapp/src/component/Article.jsx
@@ -1,18 +1,55 @@
 import React, { useState ,useEffect } from 'react'
 import ArtStyle from "../css/Article.module.css"
+import axios from "axios"
 //import "../css/Article.css"
+
 
 function Article (){
     const [count ,setCount]=useState(0)
     const [countt ,setCountt]=useState(0)
     const [name, setName] = useState("Priyanka")
+    const [loading,setloading]=useState(false)    
     //let count =0;
+
     useEffect(()=>{
       console.log("Use Effect Called")
       return()=>{
         console.log("Use Effect Cleanup")
       }
     },[name])
+
+    useState(()=>{
+
+
+
+      /*const fetchData = async()=>{
+        const response =await fetch("https://dummyjson.com/products",{
+          method:"GET"
+        })
+        const data = await response.json()  
+        console.log(data)
+
+        }catch(e){
+          console.log(e)
+        }
+
+      }/*
+      /*const fetchData =()=>{
+      console.log("Data Fetched")
+    }
+     fetchData()*/
+
+     const fetchData=async()=>{
+        try{
+          const response=await axios.get("https://dummyjson.com/products")
+          console.log(response.data)
+        }catch(error){
+          console.log(error)
+        }
+      }
+
+      fetchData()
+    },[])
 
     const handleIncrement = () => {
         //count+1;
@@ -28,6 +65,7 @@ function Article (){
   return (
     <div>
         <h2>Article</h2>
+        {loading?<h2>Loading ....</h2>:""}
         <p>During the pre-match press conference ahead of their Indian Premier League (IPL) 2026 match against the Rajasthan Royals (RR), Paras reflected on Bumrah and his evolution from a teenager to becoming a global icon.
            Advertisement
           "One value that every individual and bowler should pick up from Boom (Bumrah) is clearly his work ethic. The way he goes about it in the nets, tick marking everything that he has worked on and will be using during the game. But, in the last few years with the Indian team...he has grown into a phenomenal bowler and even more so as an individual, which has been amazing," the MI bowling coach said.
